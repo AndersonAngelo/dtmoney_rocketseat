990cc7180cbbafcd3055f48f16897f5a5ba3e744
Anderson Ângelo <anderson_angelo1@hotmail.com>
2022-01-03 05:20:35 -0300

    added feactures to the forms

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,34 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
+import incomeImg from '../../assets/entradas.svg';
+import outcomeImg from '../../assets/saidas.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+
+import { api } from '../../services/api';
+
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+  const [title, setTitle] = useState('');
+  const [value,  setValue] = useState(0);
+  const [category, setCategory] =useState('');
+  
+  function handleCreateNewTransactions(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      type, 
+      title, 
+      value, 
+      category
+    };
+
+    api.post('/transactions', data);
+  }
   return (
     <Modal
       isOpen={isOpen}
@@ -21,18 +43,52 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       >
         <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+
+      <Container onSubmit={handleCreateNewTransactions}>
         <h2>Cadastrar transação</h2>
+
         <input 
           placeholder="Título" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
         />
+
         <input 
           type="number" 
           placeholder="Valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
         />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => { setType('deposit'); }}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => { setType('withdraw'); }}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+
+        </TransactionTypeContainer>
+
         <input 
           placeholder="Categoria" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}
         />
+
         <button type="submit">
           Cadastrar
         </button>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
-  routes() {
-    this.namespace = 'api';
-
-    this.get('/transactions', () => {
-      return [
+  models: {
+    transaction: Model,
+  },
+  //adicionando dados ao banco de dados para iniciar o app com dados pré cadastrados ao DB.
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Transaction 1',
-          amount: 400,
+          title: 'Freelancer de website',
           type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-01-03 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 600,
+          createdAt: new Date('2022-01-10 12:00:00'),
         }
-      ]
+      ],
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+    //Pegando todos os dados da transaction.
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+    //recebendo e transfomando em JSON os dados do formulário e adicionando ao DB(schema)
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
